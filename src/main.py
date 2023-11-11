5617af7274bff2b2525aec1b32cbceefea29ba63
Abdirazak Yusuf <abdirazak140@gmail.com>
2023-11-11 15:28:10 -0500

    Added games

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 from cpu import CPU
+import os
 
-        
 def Emulator():
-    rom_path = r"/home/abdi/Documents/GitHub/CHIP-8-emulator/test/quirks.ch8"
-    cpu = CPU(rom_path)
-    
+    game_folder = "games"
+    game_list = os.listdir(game_folder)
 
+    print("Available Games:")
+    for i, game in enumerate(game_list, start=1):
+        print(f"{i}. {game}")
+
+    game_choice = input("What game do you want to play: ")
+    rom_path = os.path.join(game_folder, game_choice)
+
+    cpu = CPU(rom_path)
 
 if __name__ == '__main__':
     Emulator()
